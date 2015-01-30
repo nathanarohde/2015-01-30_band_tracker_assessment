dcebf98e45bc91c9fd52032cd562ebca97b1aef7
monty <nathanarohde@gmail.com>
2015-01-30 11:49:04 -0800

    Programs accepts input for bands and venues, successfully placing them in the database.

--- app.rb
+++ app.rb
@@ -5,6 +5,28 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get ('/') do
   @bands = Band.all
+  @venues = Venue.all
 
   erb(:index)
+end
+
+post ('/band_add') do
+  band_name = params['band_name']
+  Band.create({ :name => band_name})
+
+  redirect ('/')
+end
+
+post ('/venue_add') do
+  venue_location = params['venue_location']
+  Venue.create({ :location => venue_location})
+
+  redirect ('/')
+end
+
+get ('/band_edit/:id') do
+  @band = Band.find(params['id'])
+  @venues = Venue.all
+
+  erb(:band_edit)
 end
--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,11 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
+  validates(:location, {:presence => true})
+  before_save(:capitalize_location)
+
+  private
+
+  define_method(:capitalize_location) do
+    self.location=(location().capitalize())
+  end
 end
